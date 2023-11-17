43f96019e88e288e3083bb4ce89aa4de09360e46
JianHua-Deng <dengjianhua2397462613@gmail.com>
2023-11-17 00:40:11 -0500

    i guess i kinda get it

--- Project 2/main.cpp
+++ Project 2/main.cpp
@@ -2,9 +2,9 @@
 
 int main(){
 
-    std::vector<int> vec1 = {1, 4, 3, 78, 23, 50, 60, 90}; // 1 3 4 23 50 60 78 90
+    std::vector<int> vec1 = {1, 4, 3, 78, 23, 50, 60}; // 1 3 4 23 50 60 78 90
     int duration = 0;                                      // 1 3 4 23 50 60 78
-
+    std::vector<int> vec2 = {1, 4, 3, 78, 23, 50, 60, 90};
     std::cout << halfSelectionSort(vec1, duration) << std::endl;
 
     for(int i = 0; i < vec1.size(); i++){
