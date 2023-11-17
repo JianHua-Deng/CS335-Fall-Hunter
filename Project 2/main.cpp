1b9b5b6b4c004a6177361ba478a6714ac83c8e3f
JianHua-Deng <dengjianhua2397462613@gmail.com>
2023-11-17 11:37:34 -0500

    added print function

--- Project 2/main.cpp
+++ Project 2/main.cpp
@@ -1,15 +1,19 @@
 #include "HalfSelectionSort.hpp"
 
+void print(std::vector<int>& nums){
+    for(int i = 0; i < nums.size(); i++){
+        std::cout << nums[i] << ", ";
+    }
+    std::cout << std::endl;
+}
+
 int main(){
 
     std::vector<int> vec1 = {1, 4, 3, 78, 23, 50, 60}; // 1 3 4 23 50 60 78 90
     int duration = 0;                                      // 1 3 4 23 50 60 78
     std::vector<int> vec2 = {1, 4, 3, 78, 23, 50, 60, 90};
     std::cout << halfSelectionSort(vec1, duration) << std::endl;
-
-    for(int i = 0; i < vec1.size(); i++){
-        std::cout << vec1[i] << ", ";
-    }
-    std::cout << "\n";
+    print(vec1);
+    print(vec2);
     return 0;
 }
