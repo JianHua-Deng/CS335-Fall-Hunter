786e5c3118edf9f4977aff94728c504796e4927f
JianHua-Deng <dengjianhua2397462613@gmail.com>
2023-11-20 22:42:54 -0500

    normal version of buildheap

--- Project 2/HalfHeapSort.cpp
+++ Project 2/HalfHeapSort.cpp
@@ -1,16 +1,46 @@
 #include "HalfHeapSort.hpp"
-
+/*
+Details: Before starting heapsort, take the first element in the vector (at index 0) and move it to the back of the vector. This will leave index 0 open, as per the textbook implementation.
+For heapsort, you don't have to use iterators. Using array subscripts is more efficient since you'll be doing a lot of arithmetic on indexes (e.g. doubling them to find the left child, halving to find the parent).
+At the end of the sort, you should have deleted elements smaller up to but not including the median - i.e. at the end of the sort, the median should be at the root of the remaining heap.
+*/
 int halfHeapSort (std::vector<int>& nums, int& duration){
+    //nums.push_back(std::move(nums[0]));//moving the first element to the end of the vector
+    buildHeap(nums);
 
+    return -1;
 }
 
 // parameter "hole" is the index of the hole.
 // percDown precondition: value to be inserted into hole is stored in heap at index 0. The hole itself may be in an unspecified state - i.e. it doesn't matter what's in it since you'll be overwriting it anyway.
 // percDown postcondition: hole has been moved into correct place and value has been inserted into hole.
-void percDown (std::vector<int>& heap, std::vector<int>::sizetype hole){
+void percDown (std::vector<int>& heap, std::vector<int>::size_type hole){
+    int left = hole * 2 + 1;//left child index
+    int right = hole * 2 + 2;//right child index
+    int smallest = hole; //value to be inserted into hole is stored in heap at index 0
 
+    //if left child index is less than heap size, and left < smallest(index 0)
+    if(left < heap.size() && heap[left] < heap[smallest]){
+        smallest = left;
+    }
+    
+    //if right child index is less than heap size, and right < smallest(index 0)
+    if(right < heap.size() && heap[right] < heap[smallest]){
+        smallest = right;
+    }
+
+    //if smallest != hole, it means we didnt find the new spot, so we swap hole and smallest, and recursively percDown again
+    if(smallest != hole){
+        std::swap(heap[hole], heap[smallest]);
+        percDown(heap, smallest);
+    }
 }
 
 void buildHeap (std::vector<int>& heap){
+
+    for(int i = heap.size()/2 - 1; i >= 0; --i){
+        //heap[0] = std::move(heap[i]);
+        percDown(heap, i);
+    }
 
 }
--- Project 2/main.cpp
+++ Project 2/main.cpp
@@ -2,6 +2,7 @@
 #include "StandardSort.hpp"
 #include "MergeSort.hpp"
 #include "InPlaceMergeSort.hpp"
+#include "HalfHeapSort.hpp"
 void print(std::vector<int>& nums){
     for(int i = 0; i < nums.size(); i++){
         std::cout << nums[i] << ", ";
@@ -15,10 +16,11 @@ int main(){
     int duration = 0;                                  // 1 3 4 23 50 60 78
     std::vector<int> vec2 = {1, 4, 3, 78, 23, 50, 60, 90};
     std::vector<int> vec3 = {1, 4, 3, 78, 23, 50, 60, 90, 0, 60, 2, 5, 100, 154, 30, 34};
-    std::cout << "Median: "<< halfSelectionSort(vec3, duration) << ", Duration: " << duration <<std::endl;
+    //std::cout << "Median: "<< halfSelectionSort(vec3, duration) << ", Duration: " << duration <<std::endl;
     //std::cout << "Median: "<< standardSort(vec3, duration) << ", Duration: " << duration <<std::endl;
     //std::cout << "Median: "<< mergeSort(vec3, duration) << ", Duration: " << duration <<std::endl;
     //std::cout << "Median: "<< inPlaceMergeSort(vec3, duration) << ", Duration: " << duration <<std::endl;
+    std::cout << "Median: "<< halfHeapSort(vec3, duration) << ", Duration: " << duration <<std::endl;
     //print(vec1);
     //print(vec2);
     print(vec3);
